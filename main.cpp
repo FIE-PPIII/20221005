eac6badec3df555d4fd590cbbb88a88f14b398e6
Juan Bautista Nemi <80529710+jnemi2@users.noreply.github.com>
2022-10-12 10:51:41 -0300

    MOD - main.cpp

--- main.cpp
+++ main.cpp
@@ -10,12 +10,14 @@ int main()
 {
     cout<<"Hello, world!"<<endl;
 	
+	list<Tecla> teclas;
 	list<Numero> nums;
 	for (int i = 0; i < 10; i++)
 		nums.push_back(Numero(i+48));
 	list<Numero>::iterator itern = nums.begin();
 	for (int i = 0; i < nums.size(); i++) {
 		cout<<itern.operator*().getKey()<<endl;
+		teclas.push_back(*itern);
 		itern.operator++();
 	}
 	
@@ -25,7 +27,12 @@ int main()
 	list<Letra>::iterator iterl = lets.begin();
 	for (int i = 0; i < lets.size(); i++) {
 		cout<<iterl.operator*().getKey()<<endl;
+		teclas.push_back(*iterl);
 		iterl.operator++();
 	}
+	
+	cout<<endl<<"Start of list<Tecla>"<<endl;
+	for (list<Tecla>::iterator it = teclas.begin(); it != teclas.end(); it++)
+	    cout<<(*it).getKey()<<endl;
     return 0;
 }
