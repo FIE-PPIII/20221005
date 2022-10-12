3fe70d4cbe400099e59683dce6154827a704431a
Juan Bautista Nemi <80529710+jnemi2@users.noreply.github.com>
2022-10-12 10:53:15 -0300

    MOD - Numero.h

--- Numero.h
+++ Numero.h
@@ -16,8 +16,13 @@ class Numero : public Tecla {
 		Numero(char);
 		virtual ~Numero();
 		
+		char getKey();
 		void setNum(int);
 };
 
+
+inline char Numero::getKey(){
+	return this->key;
+}
 
 #endif
